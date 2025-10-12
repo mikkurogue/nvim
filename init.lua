a9269523b653ce52ebd3ff668da83204c111871f
mikkurogue <michael.lindemans@outlook.com>
2025-10-12 20:25:31 +0300

    add fidget to the thing to show that the lsp is connecting and shit

--- init.lua
+++ init.lua
@@ -24,6 +24,7 @@ v.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/akinsho/toggleterm.nvim" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/j-hui/fidget.nvim", name = "fidget.nvim" },
 })
 
 -- Add colorschemes
@@ -125,71 +126,45 @@ v.keymap.set(
 )
 
 -- Load plugins with custom  config
+-- LSP configurations
+local capabilities = require("blink.cmp").get_lsp_capabilities(v.lsp.protocol.make_client_capabilities())
+
+local function on_lsp_exit(code, signal, client_id)
+  local client = v.lsp.get_client_by_id(client_id)
+  if not client then return end
+
+  -- Don't show message on normal exit
+  if code == 0 and signal == 0 then
+    return
+  end
+
+  v.notify(
+    string.format("LSP client '%s' crashed. (code: %s, signal: %s)", client.name, tostring(code), tostring(signal)),
+    v.log.levels.WARN
+  )
+
+  v.notify(string.format("Attempting to restart LSP client: %s", client.name), v.log.levels.INFO)
+
+  local new_client_id = v.lsp.start_client(client.config)
+  if new_client_id then
+    v.notify(string.format("LSP client '%s' has been restored.", client.name), v.log.levels.INFO)
+    -- Re-attach to the current buffer
+    v.lsp.buf_attach_client(0, new_client_id)
+  else
+    v.notify(string.format("Failed to restart LSP client: %s", client.name), v.log.levels.ERROR)
+  end
+end
+
+v.lsp.enable({ "lua_ls", "biome", "rust_analyzer", "vtsls" }, {
+	capabilities = capabilities,
+    on_exit = on_lsp_exit,
+})
 require("configuration.blink-cmp")
 require("configuration.conform")
 require("configuration.treesitter")
 require("configuration.gitsigns")
 require("configuration.toggleterm")
-
-require("mini.pick").setup()
-require("mini.pairs").setup()
--- Diagnostics UI
-require("tiny-inline-diagnostic").setup({
-	preset = "powerline"
-})
-
-v.keymap.set("n", "<leader>xx", function() require("trouble").toggle("diagnostics") end,
-	{ desc = "Toggle Trouble diagnostics" })
-
--- LSP configurations
-local capabilities = require("blink.cmp").get_lsp_capabilities(v.lsp.protocol.make_client_capabilities())
-v.lsp.enable({ "lua_ls", "biome", "rust_analyzer", "vtsls" }, {
-	capabilities = capabilities
-})
-
--- Format on save with lsp configuration
-local fmt_grp = v.api.nvim_create_augroup("LspFormatOnSave", { clear = true })
-
--- This function runs when an LSP client attaches to a buffer
-v.api.nvim_create_autocmd("LspAttach", {
-	group = fmt_grp,
-	callback = function(args)
-		local bufnr = args.buf
-		local client_id = args.data.client_id
-		local client = v.lsp.get_client_by_id(client_id)
-		if not client then
-			return
-		end
-
-		if client.server_capabilities.documentFormattingProvider or client.server_capabilities.documentRangeFormattingProvider then
-			v.api.nvim_clear_autocmds({ group = fmt_grp, buffer = bufnr })
-			v.api.nvim_create_autocmd("BufWritePre", {
-				group = fmt_grp,
-				buffer = bufnr,
-				callback = function()
-					v.lsp.buf.format({
-						bufnr = bufnr,
-						async = false,
-						timeout_ms = 2000,
-					})
-				end,
-				desc = "LSP format on save",
-			})
-		end
-	end,
-})
-
--- Show lsp progress in command line
-v.api.nvim_create_autocmd("LspProgress", {
-	callback = function(args)
-		if args.data and args.data.params then
-			local msg = args.data.params.value
-			if msg and msg.kind == "end" then
-				print("rust-analyzer: " .. (msg.message or "done"))
-			end
-		end
-	end,
-})
+require("configuration.fidget")
 -- show lsp that is attached to buffer
 function _G.LspStatus()
 	local bufnr = v.api.nvim_get_current_buf()
